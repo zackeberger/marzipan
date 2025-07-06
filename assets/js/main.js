01e90a1e3f15a14545a29f844bcc427a6edffdec
Zack Berger <zackberger@OSXLAP12779.lan>
2025-07-06 12:30:59 +0200

    Add initial website scaffold.

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Simple example: show a console greeting
+console.log("Welcome to Marzipan’s website 🎸🥁");
+
+/* Future ideas:
+   - Fetch upcoming shows from a JSON or Google Sheet
+   - Add a light/dark mode toggle
+   - Integrate Bandcamp/Spotify widgets dynamically
+*/
+
